0e1393c19db5b41b0cbc527876bcf4e4376c3329
Enzo F Borges <enzo.alternativo@hotmail.com>
2025-03-11 21:11:22 -0300

    Correções Ortográficas

--- Projeto Original/projetofinal.c
+++ Projeto Original/projetofinal.c
@@ -3,15 +3,15 @@
 #include <string.h>
 #define TOTAL 100
 
-typedef struct // a estrutura mais importante do c?digo!
+typedef struct // a estrutura mais importante do código!
 {
     int matricula;
     char nome[TOTAL];
     int idade;
 } Aluno;
 
-// fun??es a ser utilizadas
-// usei "void" pq n?o retorna valor
+// funções a serem utilizadas
+// usei "void" porque não retorna valor
 void lista_alunos(Aluno alunos[TOTAL], int total);
 void consultar_aluno(Aluno alunos[TOTAL], int total);
 void adicionar_aluno(Aluno alunos[TOTAL], int *total);
@@ -25,8 +25,8 @@ int main()
     setlocale(LC_ALL, "");
 
     int opcao;
-    // essas variaveis,com exce??o de "opcao", ser?o utilizadas como parametros nas fun??es.
-    //  a struct "aluno" inicializada, ? atrav?s dela que os dados ser?o registrados.
+    // essas variáveis, com exceção de "opcao", serão utilizadas como parâmetros nas funções.
+    // a struct "aluno" inicializada, é através dela que os dados serão registrados.
     Aluno alunos[TOTAL];
     int totalGraduando = 0, totalFormado = 0; // variaveis inicializadas;
     char resposta;                            // variavel para usada para definir a continuidade do usu?rio no programa.
@@ -34,7 +34,7 @@ int main()
     do
     {
         printf("\n===========================================================\n");
-        printf("       Sistema Eletr?nico de Gest?o de Alunos - SEGA\n");
+        printf("       Sistema Eletrônico de Gestão de Alunos - SEGA\n");
         printf("===========================================================\n\n");
         printf("                    - Menu Principal -\n\n\n");
         printf("    1. Alunos (graduando)\n");
@@ -101,7 +101,7 @@ int main()
                     break;
                 default:
                     system("cls");
-                    printf("            Op??o inv?lida!\n");
+                    printf("            Opção inválida!\n");
 
                     break;
                 }
@@ -161,7 +161,7 @@ int main()
                     break;
                 default:
                     system("cls");
-                    printf("Op??o inv?lida!\n");
+                    printf("Opção inválida!\n");
                     break;
                 }
             } while (escolha != 2 && opcao != 5 && opcao != 6);
@@ -175,7 +175,7 @@ int main()
             break;
         }
 
-    } while ((opcao != 5) && (opcao != 3)); // c?digo continuar? no loop at? uma dessas condi??es ser atendida
+    } while ((opcao != 5) && (opcao != 3)); // código continuará no loop até uma dessas condições ser atendida
 
     return 0;
 }
@@ -183,16 +183,16 @@ int main()
 void consultar_aluno(Aluno alunos[TOTAL], int total)
 {
     char nome_aluno[TOTAL];
-    int encontrado = 0; // Variavel para ser usado como refer?ncia, se permanecer em 0, vai constar como "n?o encontrado".
+    int encontrado = 0; // Variável para ser usada como referência, se permanecer em 0, vai constar como "não encontrado".
 
     printf("Digite o nome do aluno:\n");
     fgets(nome_aluno, TOTAL, stdin);
-    nome_aluno[strcspn(nome_aluno, "\n")] = '\0'; // strcspn vai ler a posi??o que encontra o \n e vai substitui-lo por nulo;
+    nome_aluno[strcspn(nome_aluno, "\n")] = '\0'; // strcspn vai ler a posição que encontra o \n e vai substituí-lo por nulo;
     // isso evita de ficar registrado caracteres indesejados na string.
 
     for (int i = 0; i < total; i++)
     {
-        if (strcmp(alunos[i].nome, nome_aluno) == 0) // estrutura b?sica de compara??o de strings
+        if (strcmp(alunos[i].nome, nome_aluno) == 0) // estrutura básica de comparação de strings
         {
             system("cls");
             printf("-- Registro do aluno --\n");
@@ -207,14 +207,14 @@ void consultar_aluno(Aluno alunos[TOTAL], int total)
     if (encontrado == 0)
     {
         system("cls");
-        printf("Aluno n?o encontrado.\n");
+        printf("Aluno não encontrado.\n");
     }
 }
 
-void adicionar_aluno(Aluno alunos[TOTAL], int *total) // a variavel *total(ponteiro) ? a que est? contabilizando os alunos, "TOTAL" ? o define usado no come?o do c?digo.
-// o ponteiro "total" vai alterar as variav?is dentro da fun??o main.
+void adicionar_aluno(Aluno alunos[TOTAL], int *total) // a variável *total(ponteiro) é a que está contabilizando os alunos, "TOTAL" é o define usado no começo do código.
+// o ponteiro "total" vai alterar as variáveis dentro da função main.
 {
-    if (*total >= TOTAL) // este "if" vai garantir que n?o ultrapasse o total estipulado no inicio do codigo.
+    if (*total >= TOTAL) // este "if" vai garantir que não ultrapasse o total estipulado no início do código.
     {
         system("cls");
         printf("Limite de alunos atingido.\n");
@@ -223,14 +223,14 @@ void adicionar_aluno(Aluno alunos[TOTAL], int *total) // a variavel *total(ponte
 
     alunos[*total].matricula = *total + 1;
     printf("Digite o nome do aluno: ");
-    fgets(alunos[*total].nome, TOTAL, stdin);                       // funciona melhor para leitura de palavras, lendo tamb?m os espa?os.
+    fgets(alunos[*total].nome, TOTAL, stdin);                       // funciona melhor para leitura de palavras, lendo também os espaços.
     alunos[*total].nome[strcspn(alunos[*total].nome, "\n")] = '\0'; // Remove a nova linha
     printf("Digite a idade do aluno: ");
     scanf("%d", &alunos[*total].idade);
     getchar();
 
     (*total)++;    // adiciona mais um "aluno" na contagem total.
-    system("cls"); // diminui a polui??o visual.
+    system("cls"); // diminui a poluição visual.
     printf("Aluno adicionado com sucesso.\n");
 }
 
@@ -238,19 +238,19 @@ void remover_aluno(Aluno alunos[TOTAL], int *total)
 {
     int matricula, encontrado = 0;
 
-    printf("Digite a matr?cula do aluno a ser removido: ");
+    printf("Digite a matrícula do aluno a ser removido: ");
     scanf("%d", &matricula);
     fflush(stdin);
 
     for (int i = 0; i < *total; i++)
     {
-        if (alunos[i].matricula == matricula) // vai fazer uma busca no banco de dados para encontrar a matricula digitada anteriormente
+        if (alunos[i].matricula == matricula) // vai fazer uma busca no banco de dados para encontrar a matrícula digitada anteriormente
         {
-            for (int j = i; j < *total - 1; j++) // encontrando o aluno no "if", o "for" ser? executado;
+            for (int j = i; j < *total - 1; j++) // encontrando o aluno no "if", o "for" será executado;
             {
                 alunos[j] = alunos[j + 1];
             }
-            (*total)--; // retira esse aluno do total contabilizado at? o momento.
+            (*total)--; // retira esse aluno do total contabilizado até o momento.
             encontrado = 1;
             system("cls");
             printf("Aluno removido com sucesso.\n");
@@ -276,7 +276,7 @@ void lista_alunos(Aluno alunos[TOTAL], int total)
 {
     system("cls");
     printf("          -- Banco de alunos --\n\n");
-    printf("    NOME                    MATR?CULA \n\n");
+    printf("    NOME                    MATRÍCULA \n\n");
     for (int i = 0; i < total; i++)
     {
         printf("  %-20s  --     %03d\n", alunos[i].nome, alunos[i].matricula);
